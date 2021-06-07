6549948220fb660d524e5489a0880c3936b82603
Stefano Giraldi <stefano.giraldi@gmail.com>
2021-06-07 07:41:01 +0200

    Changed router history mode to hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from 'vue-router'
+import { createWebHashHistory, createRouter } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Tabs from '../views/Tabs.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/mpay-link-builder/'),
+  history: createWebHashHistory('/mpay-link-builder/'),
   routes,
 })
 
